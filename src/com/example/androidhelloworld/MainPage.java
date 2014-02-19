943e0402c1c7c043272586c72f5c091b52276fe6
Jonathan Lei <jonathan.lei01@gmail.com>
2014-02-18 16:00:39 -0800

    added comments to main page

--- src/com/example/androidhelloworld/MainPage.java
+++ src/com/example/androidhelloworld/MainPage.java
@@ -11,43 +11,50 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainPage extends Activity {
-	
+
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-	
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
-        
-        Button uselessButton = (Button)findViewById(R.id.button2);
-        uselessButton.setOnClickListener(new OnClickListener(){
-        	
-        	@Override
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_page);
+
+		// Useless Button
+		Button uselessButton = (Button) findViewById(R.id.button2);
+		// Onlclick method
+		uselessButton.setOnClickListener(new OnClickListener() {
+
+			@Override
 			public void onClick(View v) {
-				TextView title = (TextView)findViewById(R.id.helloWorld);
+				TextView title = (TextView) findViewById(R.id.helloWorld);
+				// Setting the string
 				title.setText("My First App");
-				
+
 			}
-			
-        });
-        
-    }
-    
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, Hello_Name.class);
-        EditText editText = (EditText) findViewById(R.id.nameHere);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
 
+		});
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_page, menu);
-        return true;
-    }
-    
+	}
+
+	// Send name method.
+	public void sendMessage(View view) {
+		// Create new Intent
+		Intent intent = new Intent(this, Hello_Name.class);
+		// Find the text area.
+		EditText editText = (EditText) findViewById(R.id.nameHere);
+		// toString
+		String message = editText.getText().toString();
+		// Put method into Intent.
+		intent.putExtra(EXTRA_MESSAGE, message);
+		// Switch activities.
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_page, menu);
+		return true;
+	}
+
 }
